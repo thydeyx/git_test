26577a306ef61fab41a2ad4af87dd71995dca990
lucas <hanyi_tang@apple.com>
2018-11-12 15:59:14 -0800

    add a.py

---
+++ a.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#
+#        Author : TangHanYi
+#        E-mail : thydeyx@163.com
+#   Create Date : 2018-11-12 15时58分59秒
+# Last modified : 2018-11-12 15时59分02秒
+#     File Name : a.py
+#          Desc :
+
+
+if __name__ == "__main__":
+    s = Solution()

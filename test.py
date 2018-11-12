9da53f8f8285033b31bc67953e256d82bbf97dfc
lucas <hanyi_tang@apple.com>
2018-11-12 15:47:22 -0800

    create class

--- test.py
+++ test.py
@@ -3,10 +3,14 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2018-11-12 15时39分26秒
-# Last modified : 2018-11-12 15时39分31秒
+# Last modified : 2018-11-12 15时45分05秒
 #     File Name : test.py
 #          Desc :
 
+class Solution:
+
+    def __init__():
+        pass
 
 if __name__ == "__main__":
     s = Solution()
